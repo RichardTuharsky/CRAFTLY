9a57b25e4669c61cdbf61d542f97eca533d28420
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2024-01-15 19:48:05 +0100

    x

--- work.sql
+++
@@ -1,6 +0,0 @@
-CASE
-    WHEN coffee_usage = 'zero' then coffee_no_buyer
-    WHEN coffee_usage = 'light' then coffee_light_buyer
-    WHEN coffee_usage = 'medium' AND business_mode = 'B2C' then coffee_medium_buyer_b2c
-    WHEN coffee_usage = 'medium' AND business_mode = 'B2B2C' then coffee_medium_buyer_b2b2c
-END AS PromotionMember_ID
